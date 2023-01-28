5115a6f86ee02db72528d2106a755ce76ed1ef66
sepponevalainen <S.Nevalainen@gmail.com>
2023-01-28 12:50:57 +0200

    Improved static image and text components and created first version of kolmikarki_front_page component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Kolmikarki_front_page from './components/Kolmikarki_front_page.js';
-import Background_image_front_page from './images/Background_image_front_page.png';
-import Static_image from './components/static_image.js';
-import Static_text from './components/static_text.js';
+import Kolmikarki_front_page from './components/kolmikarki_front_page.js';
+
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage : `url(${Background_image_front_page})`, height:"100vh"}}>
-      <Static_text name= "Kolmikärki" />
+    <div className="App">
+      <Kolmikarki_front_page />
       </div>
   );
 }
--- src/components/kolmikarki_front_page.js
+++ src/components/kolmikarki_front_page.js
@@ -1,5 +1,15 @@
+import Background_image_front_page from './../images/Background_image_front_page.png';
+import Dragonfly_image from './../images/Dragonfly.png';
+import Static_image from './static_image.js';
+import Static_text from './static_text.js';
+
 function Kolmikarki_front_page(props) {
-    return <h1>kfp, {props.name}</h1>;
+  return (
+    <div className="App" style={{backgroundImage : `url(${Background_image_front_page})`, height:"100vh"}}>
+      <Static_text name= "Kolmikärki" />
+      <Static_image name={Dragonfly_image}/>
+      </div>
+  );
   }
 
   export default Kolmikarki_front_page
