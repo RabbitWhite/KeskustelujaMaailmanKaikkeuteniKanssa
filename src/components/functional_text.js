ff6aa037c6bb2de79faab2d23a083501d64f7b2a
Nevalainen Seppo <seppo.nevalainen@karelia.fi>
2023-02-05 15:52:36 +0200

    Started implementing functional text component.

--- src/components/functional_text.js
+++ src/components/functional_text.js
@@ -1,3 +1,6 @@
 function functional_text(props) {
+  if (props.type == "LINK")
+    <a href={props.link} target={props.target}>{props.text}</a>
+  else
     return <h1>ft, {props.name}</h1>;
   }
--- src/components/kolmikarki_front_page.js
+++ src/components/kolmikarki_front_page.js
@@ -1,15 +1,3 @@
-import Background_image_front_page from './../images/Background_image_front_page.png';
-import Dragonfly_image from './../images/Dragonfly.png';
-import Static_image from './static_image.js';
-import Static_text from './static_text.js';
-
-function Kolmikarki_front_page(props) {
-  return (
-    <div className="App" style={{backgroundImage : `url(${Background_image_front_page})`, height:"100vh"}}>
-      <Static_text name= "Kolmikärki" />
-      <Static_image name={Dragonfly_image}/>
-      </div>
-  );
-  }
+import { Kolmikarki_front_page } from "./Kolmikarki_front_page.1";
 
   export default Kolmikarki_front_page
