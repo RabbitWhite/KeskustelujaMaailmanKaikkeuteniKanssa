6857410eed3d0616274f7232bd22e973c977ad7b
Nevalainen Seppo <seppo.nevalainen@karelia.fi>
2023-02-11 23:01:18 +0200

    Initial draft for poem data reading.

--- src/components/kolmikarki_front_page.js
+++ src/components/kolmikarki_front_page.js
@@ -3,15 +3,39 @@ import Functional_image from "./functional_image"
 import { Fragment } from "react";
 
 import DFImage from "./../images/Dragonfly.png";
+import BGImage from "./../images/Background_image_front_page.png";
 
-
+import Poems from "./../data/Poems.json";
 
 export default function Kolmikarki_front_page(props) {
     return (
+        /*
         <Fragment>
-        <Functional_text tag="h1" class="alert" style={{right: `80%`}} newText="Kolmikärki2"/>
-        <Functional_image class="fixed" style={{right: `80%`, width:`20%`, height:`20%`}} newImage={DFImage}/>
+            <div className="box" style={{ backgroundImage: `url(${BGImage})` }}>
+                <Functional_text tag="h1" class="title" style={{right: `80%`}} newText="Kolmikärki2"/>
+                <Functional_image class="fixed" style={{right: `80%`, width:`20%`, height:`20%`}} newImage={DFImage}/>
+            </div>
         </Fragment>
+        */
+        <>
+        <div>
+        <table border="2">
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Department</th>
+                </tr>
+                // Mapping array of objects
+                {Poems.poemsData && Poems.poemsData.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.name}</td>
+                        <td>{item.lines}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+    </>
     )
 }
 
