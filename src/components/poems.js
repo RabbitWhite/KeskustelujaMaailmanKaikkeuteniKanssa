c266cff77148ba369dac1fd91f8bd1d22ce78757
Nevalainen Seppo <seppo.nevalainen@karelia.fi>
2023-07-10 11:07:52 +0300

    Implemented functionality for reading local JSON poem file.

--- src/components/poems.js
+++ src/components/poems.js
@@ -1,9 +1,13 @@
+import data from './../data/Poems.json'
+
 const poems = [
   ['Aral', '<pre class="poemtext">\n\nAral\n\nKuivettuneet kanavat,\nsuolaantuneet silmät.\n\nKöli dyynin harjan päällä,\nkeikkumassa\nkeulakuva aurinkoa kohti.\n\nPolte huulilla,\nsen tippuvat rohteet.\n\n  </pre>'],
   ['Kalantakoja','<pre class="poemtext">\n\nKalantakoja\n\nKuivettuneet kanavat,\nsuolaantuneet silmät.\n\nKöli dyynin harjan päällä,\nkeikkumassa\nkeulakuva aurinkoa kohti.\n\nPolte huulilla,\nsen tippuvat rohteet.\n\n  </pre>'],
   ['Tunnustuainen','<pre class="poemtext">\n\nTunnustuainen\n\nKuivettuneet kanavat,\nsuolaantuneet silmät.\n\nKöli dyynin harjan päällä,\nkeikkumassa\nkeulakuva aurinkoa kohti.\n\nPolte huulilla,\nsen tippuvat rohteet.\n\n  </pre>'],
   [],
 ];
+
+const poemsJSON = data.poemsData 
 
 function createMarkup(currentPoemName) {
     let currentPoemCell = poems.find((poemsArray) => poemsArray.includes(currentPoemName))
@@ -14,5 +18,11 @@ function createMarkup(currentPoemName) {
 }
 
 export default function Poems(props) {
+  let index = 0
+  if (props.currentPoem !== "default") 
+    index = poemsJSON.findIndex(item => item.name === props.currentPoem)
+  
+  let thePoem = poemsJSON[index].start
+  console.log(thePoem)
     return <div className="poemcontainer" dangerouslySetInnerHTML={createMarkup(props.currentPoem)} />;
 }
