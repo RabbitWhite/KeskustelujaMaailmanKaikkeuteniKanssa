7977b0b5255b7ef072862c08ef6adbaf03f1a1a5
Nevalainen Seppo <seppo.nevalainen@karelia.fi>
2023-07-11 12:40:54 +0300

    Started implementing kinpoems functionality.

--- src/components/poems.js
+++ src/components/poems.js
@@ -1,28 +1,30 @@
-import data from './../data/Poems.json'
+import {Link, useParams } from "react-router-dom";
 
+import data from './../data/Poems.json'
+/*
 const poems = [
   ['Aral', '<pre class="poemtext">\n\nAral\n\nKuivettuneet kanavat,\nsuolaantuneet silmät.\n\nKöli dyynin harjan päällä,\nkeikkumassa\nkeulakuva aurinkoa kohti.\n\nPolte huulilla,\nsen tippuvat rohteet.\n\n  </pre>'],
   ['Kalantakoja','<pre class="poemtext">\n\nKalantakoja\n\nKuivettuneet kanavat,\nsuolaantuneet silmät.\n\nKöli dyynin harjan päällä,\nkeikkumassa\nkeulakuva aurinkoa kohti.\n\nPolte huulilla,\nsen tippuvat rohteet.\n\n  </pre>'],
   ['Tunnustuainen','<pre class="poemtext">\n\nTunnustuainen\n\nKuivettuneet kanavat,\nsuolaantuneet silmät.\n\nKöli dyynin harjan päällä,\nkeikkumassa\nkeulakuva aurinkoa kohti.\n\nPolte huulilla,\nsen tippuvat rohteet.\n\n  </pre>'],
   [],
 ];
-
-const poemsJSON = data.poemsData 
+*/
+const poemsJSON = data.poemsData
 
 function createMarkup(currentPoemName) {
-    let currentPoemCell = poems.find((poemsArray) => poemsArray.includes(currentPoemName))
-    if (currentPoemCell != undefined)
-      return {__html:  currentPoemCell[1]};
+    let currentPoemCell = poemsJSON.findIndex(item => item.name === (currentPoemName))
+    console.log("ho" + currentPoemCell)
+    if (currentPoemCell != -1)
+      return {__html:  poemsJSON[currentPoemCell].content};
     else 
-      return {__html:  poems[0][1]};
+      return {__html:  poemsJSON[0].content};
 }
 
 export default function Poems(props) {
+  const {id} = useParams()
   let index = 0
   if (props.currentPoem !== "default") 
     index = poemsJSON.findIndex(item => item.name === props.currentPoem)
-  
-  let thePoem = poemsJSON[index].start
-  console.log(thePoem)
-    return <div className="poemcontainer" dangerouslySetInnerHTML={createMarkup(props.currentPoem)} />;
+
+    return <div className="poemcontainer" dangerouslySetInnerHTML={createMarkup(props.currentPoem)}/>;
 }
