b5b69a7d9637734b79e5b1b2330c69f9a0a74b15
Nevalainen Seppo <seppo.nevalainen@karelia.fi>
2024-07-24 18:44:29 +0300

    Continued implementing meadia queries for scaling text to different screens.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ root.render(
   //</React.StrictMode>
 );
 
+console.log('height default :'+window.visualViewport.height)
+console.log('width default :'+window.visualViewport.width)
+
+window.addEventListener('resize',(e)=>{         
+      console.log( `width: ${e.target.visualViewport.width}px`);
+      console.log( `height: ${e.target.visualViewport.height}px`);
+ });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
