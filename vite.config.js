2d24c89901bbacef4dca40bbcb701883aa5a7ba6
RabbitWhite <S.Nevalainen@gmail.com>
2026-03-29 13:36:42 +0300

    Verify Vite build, remove redundant public/index.html

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/KeskustelujaMaailmanKaikkeuteniKanssa/',
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' },
+    },
+  },
 })
